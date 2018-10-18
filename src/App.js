7a16cd3f62fc0298663f86ba75962b30f66d279c
Zeeroh0 <jon87tyler@gmail.com>
2018-10-18 15:31:07 -0700

    initial build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import Calendar from "./components/Calendar";
+
+import "./App.css";
+
+
+const events = [
+  { title: 'Event 1', date: '2018-10-01T00:00:00', subtitle: 'Subtitle 1' },
+  { title: 'Event 2', date: '2018-10-11T00:00:00', subtitle: 'Subtitle 2' },
+  { title: 'Event 3', date: '2018-10-21T00:00:00', subtitle: 'Subtitle 3' },
+  { title: 'Event 4', date: '2018-10-31T00:00:00', subtitle: 'Subtitle 4' },
+];
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Calendar events={events} />
       </div>
     );
   }
